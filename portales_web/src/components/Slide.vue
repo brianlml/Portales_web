e2115369371ff67c8186953980576013901ef790
aleida <naleida048@gmail.com>
2020-01-15 20:53:22 -0400

    poniendo rutas en casa

--- portales_web/src/components/Slide.vue
+++ portales_web/src/components/Slide.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
    <div>
-
 <!-- Header -->
 <header class="bg-dark text-center py-5 mb-4">
   <div class="container">
@@ -15,7 +14,9 @@
     <!-- Team Member 1 -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/figs.jpg" class="card-img-top" alt="...">
+        <b-dropdown-item to="/figs">
+          <img src="../assets/figs.jpg" class="card-img-top" alt="...">
+        </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Figs</h5>
           <div class="card-text text-black-50">Acerca de Figs</div>
@@ -25,7 +26,9 @@
     <!-- Team Member 2 -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/nebula.jpg" class="card-img-top" alt="...">
+        <b-dropdown-item to="/nebula">
+          <img src="../assets/nebula.jpg" class="card-img-top" alt="...">
+        </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Nebula</h5>
           <div class="card-text text-black-50">Acerca de Nebula</div>
@@ -35,7 +38,9 @@
     <!-- Team Member 3 -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/mandarina.jpg" class="card-img-top" alt="...">
+        <b-dropdown-item to="/mandarina">
+          <img src="../assets/mandarina.jpg" class="card-img-top" alt="...">
+        </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Mandarina</h5>
           <div class="card-text text-black-50">Acerca de Mandarina</div>
@@ -45,7 +50,9 @@
     <!-- Team Member 4 -->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/410_roof_top.jpg" class="card-img-top" alt="...">
+         <b-dropdown-item to="rooftop">
+           <img src="../assets/410_roof_top.jpg" class="card-img-top" alt="...">
+         </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">410 Rooftop</h5>
           <div class="card-text text-black-50">Acerca de 410Rooftop</div>
@@ -55,7 +62,9 @@
     <!--team 5-->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/avalon.jpg" class="card-img-top" alt="...">
+        <b-dropdown-item to="/avalon">
+          <img src="../assets/avalon.jpg" class="card-img-top" alt="...">
+        </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Avalon</h5>
           <div class="card-text text-black-50">Acerca de Avalon</div>
@@ -65,7 +74,9 @@
     <!--team 6-->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/mangos.jpg" class="card-img-top" alt="...">
+        <b-dropdown-item to="/mangos">
+          <img src="../assets/mangos.jpg" class="card-img-top" alt="...">
+        </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Mangos</h5>
           <div class="card-text text-black-50">Acerca de Mangos</div>
@@ -75,7 +86,9 @@
     <!--team 7-->
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="../assets/music_hall.png" class="card-img-top" alt="...">
+        <b-dropdown-item to="/music">
+          <img src="../assets/music_hall.png" class="card-img-top" alt="...">
+        </b-dropdown-item>
         <div class="card-body text-center">
           <h5 class="card-title mb-0">Music Hall</h5>
           <div class="card-text text-black-50">Acerca de Music Hall</div>
