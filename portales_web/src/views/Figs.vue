0307c8e7b1388b8007e96d2d7fb14d26ffd57900
aleida <naleida048@gmail.com>
2020-01-27 15:07:54 -0400

    subiendo fotografias de portada

--- portales_web/src/views/Figs.vue
+++ portales_web/src/views/Figs.vue
@@ -1,20 +1,73 @@
 <template>
 <div>
-  <b-embed
-    type="iframe"
-    aspect="16by9"
-    src="https://video.fvvi1-2.fna.fbcdn.net/v/t39.24130-2/79430914_146422709992630_1471510888553595902_n.mp4?_nc_cat=104&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&_nc_ohc=STs-dsLFgHAAX9Q_gVt&_nc_ht=video.fvvi1-2.fna&oh=0525a88949c95f7da4a235be024cc9d9&oe=5EA47EF6"
-  ></b-embed>
-  <div class="centrando mx-10 my-10">
-    <b-alert show variant="light">
-      <p> ¡Figs ya esta en Cochabamba, la mejor manera para salir de  fiesta!</p>
-      <p>Ambientes exclusivos de calidad para disfrutar de un buen FIN DE SEMANA.</p>
-      <p>Excelente ubicación  para comenzar la noche (Av. Pando, Pasaje Ovidio Suárez)</p>
-    </b-alert>
+  <header class="bg-dark py-5 mb-5">
+    <div class="container h-100">
+          <h1 class="display-4 text-white mt-5 mb-2">
+            <img src="../assets/logotipofigs.png">
+          </h1>
+    </div>
+  </header>
+ <b-container>
+  <b-row class="bv-example-row">
+    <b-col>
+        <img src="../assets/fig_8.jpg" alt="" height="400px" width="500px">
+    </b-col>
+    <b-col>
+        <div>
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mb-5">
+        <h2>Acerca de FIGS.</h2>
+        <hr>
+        <p>¡Figs ya esta en Cochabamba, la mejor manera para salir de  fiesta! 
+Ambientes exclusivos de calidad para disfrutar de un buen FIN DE SEMANA.
+Excelente ubicación  para comenzar la noche (Av. Pando, Pasaje Ovidio Suárez).
+</p>
+        <p>Eso no es todo, en Figs disfrutaras la variedad de musica para bailar y sentir el ritmo en todo el cuerpo además de las distintas temáticas que se preparan.</p>
+      </div>
+      <div class="col-md-4 mb-5">
+        <h2>Contacto</h2>
+        <hr>
+        <address>
+          <strong>Start Bootstrap</strong>
+          <br>3481 Melrose Place
+          <br>Beverly Hills, CA 90210
+          <br>
+        </address>
+        <address>
+          <abbr title="Phone">P: </abbr>
+          76964063
+          <br>
+          <abbr title="Email">E: </abbr>
+          <a href="mailto:#">productora.psyko@gmail.com</a>
+        </address>
+      </div>
+    </div>
   </div>
+        </div>
+    </b-col>
+  </b-row>
+</b-container>
+<div>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
 </div>
 </template>
-
 <script>
 export default {
 
@@ -22,9 +75,7 @@ export default {
 </script>
 
 <style>
-.centrando{
-  text-align: center
-}
+
 
 
 </style>
--- portales_web/src/views/Mandarina_bar.vue
+++ portales_web/src/views/Mandarina_bar.vue
@@ -7,6 +7,48 @@
       </carousel-slide>
   </carousel>
 </div>
+<b-container>
+  <b-row class="bv-example-row">
+    <b-col>
+        <img src="../assets/mandar_2.jpg" alt="" height="400px" width="500px">
+    </b-col>
+    <b-col>
+        <div>
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mb-5">
+        <h2>Acerca de FIGS.</h2>
+        <hr>
+        <p>¡Figs ya esta en Cochabamba, la mejor manera para salir de  fiesta! 
+Ambientes exclusivos de calidad para disfrutar de un buen FIN DE SEMANA.
+Excelente ubicación  para comenzar la noche (Av. Pando, Pasaje Ovidio Suárez).
+</p>
+        <p>Eso no es todo, en Figs disfrutaras la variedad de musica para bailar y sentir el ritmo en todo el cuerpo además de las distintas temáticas que se preparan.</p>
+      </div>
+      <div class="col-md-4 mb-5">
+        <h2>Contacto</h2>
+        <hr>
+        <address>
+          <strong>Start Bootstrap</strong>
+          <br>3481 Melrose Place
+          <br>Beverly Hills, CA 90210
+          <br>
+        </address>
+        <address>
+          <abbr title="Phone">P: </abbr>
+          76964063
+          <br>
+          <abbr title="Email">E: </abbr>
+          <a href="mailto:#">productora.psyko@gmail.com</a>
+        </address>
+      </div>
+    </div>
+  </div>
+        </div>
+    </b-col>
+  </b-row>
+</b-container>
 
   </div>
 </template>
