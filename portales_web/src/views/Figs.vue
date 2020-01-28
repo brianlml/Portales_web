7bab025c297d2f30b1af49db174b4ff90ff968ed
aleida <naleida048@gmail.com>
2020-01-28 11:26:48 -0400

    nuevos logos

--- portales_web/src/views/Figs.vue
+++ portales_web/src/views/Figs.vue
@@ -50,21 +50,22 @@ Excelente ubicación  para comenzar la noche (Av. Pando, Pasaje Ovidio Suárez).
   </b-row>
 </b-container>
 <div>
-  <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
+   <div>
+    <h4>Top and Bottom</h4>
+    <b-card-group deck>
+      <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-top>
+        <b-card-text>
+          Some quick example text to build on the card and make up the bulk of the card's content.
+        </b-card-text>
+      </b-card>
 
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
+      <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-bottom>
+        <b-card-text>
+          Some quick example text to build on the card and make up the bulk of the card's content.
+        </b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
 </div>
 </div>
 </template>
