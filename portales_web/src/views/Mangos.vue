bec819c2184c053f271e6f01e7d44ac21ff34ff2
Brian Coria <brian_coria@outlook.com>
2020-01-28 16:42:38 -0400

    cambios pequeños

--- portales_web/src/views/Mangos.vue
+++ portales_web/src/views/Mangos.vue
@@ -53,7 +53,7 @@
     <div class="row">
       <div class="col-md-4 mb-5">
         <div class="card h-100">
-          <img class="card-img-top" src="../assets/mangos.jpg" alt="" style="width:350px; height:200px;">
+          <img class="card-img-top" src="../assets/mangos/img1.jpg" alt="" style="width:350px; height:200px;">
           <div class="card-body">
             <h4 class="card-title">Card title </h4>
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
@@ -61,12 +61,8 @@
         </div>
       </div>
       <div class="col-md-4 mb-5">
-        <div class="card h-100">
+        <div class="card h-50">
           <img class="card-img-top" src="../assets/mangos.jpg" alt="" style="width:350px; height:200px;">
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-          </div>
         </div>
       </div>
       <div class="col-md-4 mb-5">
